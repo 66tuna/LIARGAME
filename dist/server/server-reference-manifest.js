3311d12dabf79481ce5dd111fed0c0a82d5ac2d0
66tuna <66tuna1018@gmail.com>
2024-01-18 01:55:23 +0900

    Modify distDir

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"X0ftdM/aalFjGcGPNhXbH5zJbBAnlaxz5z+DRidwyKE=\"}"
--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
   },
   basePath: isProduction ? "/LIARGAME" : "",
   assetPrefix: isProduction ? "/LIARGAME/" : "",
+  output: "export",
   distDir: 'dist'
 };
 
