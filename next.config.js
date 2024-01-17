1475a763cd4e759760aaae28e9866c0c2c8db70a
66tuna <66tuna1018@gmail.com>
2024-01-17 10:46:53 +0900

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
-const repository = "https://66tuna.github.io/LIARGAME";
-
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  basePath: "/LIARGAME",
 };
-
-export const prefix = process.env.NODE_ENV === "production" ? repository : "";
 
 module.exports = nextConfig;
