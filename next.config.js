5f86f26fe58aaf450bf87127a066b2fcf829e38b
66tuna <66tuna1018@gmail.com>
2024-01-17 10:57:25 +0900

    mnodfiy package.js and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  basePath: "/LIARGAME",
+  compiler: {
+    styledComponents: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    loader: "akamai",
+    path: isProduction ? "https://66tuna.github.io" : "http://localhost:3000",
+  },
+  assetPrefix: isProduction ? "/LIARGAME" : "",
 };
 
 module.exports = nextConfig;
