907511a18835d1b374432c82dce7eca9a0e0c0e3
66tuna <66tuna1018@gmail.com>
2024-01-18 01:20:42 +0900

    Modify next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,26 +1,24 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
 
-// const nextConfig = {
-//   basePath: `/LIARGAME`,
-// }
-
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
+    domains: ['i.postimg.cc'],
+    unoptimized: true,
     loader: "akamai",
     path: isProduction ? "https://66tuna.github.io" : "http://localhost:3000",
   },
   basePath: isProduction ? "/LIARGAME" : "",
   assetPrefix: isProduction ? "/LIARGAME/" : "",
-  output: "export", // 이 부분 추가
+  distDir: 'dist'
 };
 
 module.exports = isProduction
   ? {
-      ...nextConfig,
-    }
+    ...nextConfig,
+  }
   : nextConfig;
