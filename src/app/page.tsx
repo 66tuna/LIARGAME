67b7e7a899e5ddffb246560ae9a9ef399522d60f
66tuna <66tuna1018@gmail.com>
2024-01-18 15:04:03 +0900

    Revert "Updates"
    This reverts commit d9d1ce44a9dfc3c1d1460d6b765b88871da8567d.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import CategorySetting from "../../components/setting/CategorySetting";
+import PlayerSettings from "../../components/setting/PlayerSettings";
+
+export default function Home() {
+  return (
+    <div>
+      <PlayerSettings />
+      <CategorySetting />
+    </div>
+  );
+}
